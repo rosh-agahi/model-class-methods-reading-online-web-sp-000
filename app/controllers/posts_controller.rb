d161ccfcb55edb50de4c054117071618e3455ca4
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-09 06:51:06 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,10 @@ def index
   # provide a list of authors to the view for the filter control
   @authors = Author.all
  
-  # filter the @posts list based on user input
-  if !params[:author].blank?
-    @posts = Post.where(author: params[:author])
-  elsif !params[:date].blank?
+      # filter the @posts list based on user input
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
     if params[:date] == "Today"
       @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
     else
