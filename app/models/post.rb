a164b460223354624a24945596b7d9b538d56986
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-09 06:51:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -20,5 +20,11 @@ end
     self.title = self.title.titlecase
   end
   
-  
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+   
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 end
